5dbc53d95e015e461a341f7ed06ea7907b96cc27
Rahul Chaudhari_ <rahulgc545@gmail.com>
2024-11-09 15:16:23 +0530

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-fn main() {
- println!("{}", is_even(29));
+// structs
+
+struct Rect {
+    width: u32,
+    height: u32,
+
 }
 
-fn is_even(num: i32) -> bool {
- if num % 2 == 0{
-    return true
- }
- return false
+impl Rect {
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+
+    fn perimeter(&self) -> u32 {
+       2 * ( self.width + self.height)
+    }
 }
- 
+
+fn main(){
+  let rect1 = Rect{
+    width: 30,
+    height:50,
+    // you cannot define the function inside the stuct we need to just implement it and we can use it later however
+  };
+
+  println!("The area is: {}", rect1.area());
+  println!("The perimeter is: {}", rect1.perimeter())
+}
+
+
