1488cfbcde89376d8576e5051e36e9f25b303125
Rahul Chaudhari_ <rahulgc545@gmail.com>
2024-11-11 18:41:23 +0530

    result enum

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
-//options and results
+// // result enum
+// use std::fs;
 
-enum CustomOption{
-    Some(i32),
-    None,
+// fn main (){
+//     let greeting_file_result = fs::read_to_string("src/hello.txt");
+
+//     match greeting_file_result{
+//     Ok(file_content) => {
+//         println!("File read successfully! It says: {}",file_content )
+//              },
+//     Err(error)=>{
+//         println!("Failed to read file! Error: {}", error)
+//             }
+//     }
+// }
+
+ 
+use std::fs::read_to_string;
+
+fn main(){
+    let ans = read_from_file_rahul(String::from("src/hello.txt"));
+
+    match ans {
+        Ok(contents) => println!("{}", contents),
+        Err(e) => println!("{}", e),
+    }
 }
 
-fn find_first_a(s: String)-> CustomOption {
-    for ( index,char) in s.chars().enumerate() {
-    if char == 'a' {
-            return CustomOption::Some(index as i32);
-        }
+fn read_from_file_rahul (file_path: String) -> Result<String,String> {
+    let result = read_to_string(file_path);
+
+    match result {
+        Ok(data) => Ok(data),
+        Err(_) => Err(String::from("file not read"))
     }
-     return CustomOption::None;
-}
-
-fn main() {
-
-    let index = find_first_a(String::from("rahul"));
-
-    match index {
-          CustomOption::Some(value) => println!("index is {}", value ),
-          CustomOption::None => println!("a was not found ")  
-    }
-
+    
 }
