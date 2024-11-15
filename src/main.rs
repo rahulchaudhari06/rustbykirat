c6ae9da9c9c7776332a0b952feb72665085cc20e
Rahul Chaudhari_ <rahulgc545@gmail.com>
2024-11-15 23:49:59 +0530

    borrowing, references

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+/*
 
  fn main(){
     let mut s1: String = String::from("Hello memory leak");
@@ -17,4 +18,25 @@ fn pint_str(s2: String)-> String{
     return s2;
 } 
 
+this code is ugly as we need to return the value and the ownership keeps jumping too much 
 
+*/
+// We should use references and borrowing instead
+
+
+
+fn main(){
+    let mut s1 = String::from("hey there!");
+    // let s2 = &s1;
+
+    println!("{}", s1);
+    do_something( &mut s1);
+    // println!("{}", s1);
+
+}
+
+fn do_something(s2: &mut String) {
+    s2.push_str(" how are you?");
+    println!("{}", s2); //s2 owns the value
+
+}
